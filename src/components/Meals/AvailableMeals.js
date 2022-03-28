9fd67a8555d374ab26c3ca5f31c3157c1121c60c
Iznogud112 <zare@live.co.uk>
2022-03-28 21:08:42 +0200

    Added Cart in Cart component and Input in UI component

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,4 +1,7 @@
+import Card from "../UI/Card";
+
 import classes from "./AvailableMeals.module.css";
+import MealItem from "./MealItem/MealItem";
 
 const DUMMY_MEALS = [
   {
@@ -28,11 +31,21 @@ const DUMMY_MEALS = [
 ];
 
 function AvailableMeals() {
-  const mealList = DUMMY_MEALS.map((meal) => <li>{meal.name}</li>);
+  const mealList = DUMMY_MEALS.map((meal) => (
+    <MealItem
+      id={meal.id}
+      key={meal.id}
+      name={meal.name}
+      description={meal.description}
+      price={meal.price}
+    />
+  ));
 
   return (
     <section className={classes.meals}>
-      <ul>{mealList}</ul>
+      <Card>
+        <ul>{mealList}</ul>
+      </Card>
     </section>
   );
 }
