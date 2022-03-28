9ee31b792ef1a83a734cb3f0b69a2218afb07f91
Iznogud112 <zare@live.co.uk>
2022-03-28 19:38:22 +0200

    Added Meals, MealsSummary and AvailableMeals in Meals component

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -2,13 +2,14 @@ import React from "react";
 
 import mealImage from "../../assets/meals.jpg";
 import classes from "./Header.module.css";
+import HeaderCartButton from "./HeaderCartButton";
 
 function Header(props) {
   return (
     <React.Fragment>
       <header className={classes.header}>
         <h1>Web Kitchen</h1>
-        <button>Cart</button>
+        <HeaderCartButton />
       </header>
       <div className={classes["main-image"]}>
         <img src={mealImage} alt="Table with food!" />
